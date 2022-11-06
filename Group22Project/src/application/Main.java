6c3a1b6c1854ce95896c4ce1c9521274e7da3aed
Andrew Hall <andrew.hall1@ucalgary.ca>
2022-11-06 15:44:35 -0700

    Deleting some comments

--- Group22Project/src/application/Main.java
+++ Group22Project/src/application/Main.java
@@ -1,9 +1,6 @@
 package application;
 	
 import java.io.FileInputStream;
-
-//Needs to be committed
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
