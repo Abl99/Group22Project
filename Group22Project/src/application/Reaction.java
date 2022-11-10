82aae76e8db244e10c44ced34cfdfe6979de334d
Macaella <macaella.roy@ucalgary.ca>
2022-11-09 18:18:26 -0700

    Reaction Class
    created balancedReaction method (checks if a reaction is balanced) and
    getLimitingReagent method. new constructor added to include masses of
    products and reactants. both new methods were tested and work as
    expected.

--- Group22Project/src/application/Reaction.java
+++ Group22Project/src/application/Reaction.java
@@ -3,14 +3,15 @@ package application;
 import java.util.ArrayList;
 
 public class Reaction extends Molecule {
+	// TODO encapsulate
 	ArrayList<Integer> reactantCoeff;
 	ArrayList<String> reactantMolecules;
 	ArrayList<Integer> productCoeff;
 	ArrayList<String> productMolecules;
+	ArrayList<Double> reactantMasses; 
+	ArrayList<Double> productMasses;
+	String limitingReagent;
 
-	// when making an instance of Reaction, what do you need to provide?
-	// ArrayList reacants = coeff, reactant (molecule), coeff, reactant,...
-	// ArrayList products = coeaff, product (molecule), coeff, product,...
 	public Reaction(String m) {
 		super(m);
 		// TODO make this actually sensible
@@ -21,6 +22,21 @@ public class Reaction extends Molecule {
 		this.reactantMolecules = reactantMolecules;
 		this.productCoeff = productCoeff;
 		this.productMolecules = productMolecules;
+		this.reactantMasses = new ArrayList<Double>();
+		this.productMasses = new ArrayList<Double>();
+		limitingReagent = "";
+	}
+	
+	public Reaction(ArrayList<Integer> reactantCoeff, ArrayList<String> reactantMolecules,
+			ArrayList<Integer> productCoeff,ArrayList<String> productMolecules,
+			ArrayList<Double> reactantMasses, ArrayList<Double> productMasses) {
+		this.reactantCoeff = reactantCoeff;
+		this.reactantMolecules = reactantMolecules;
+		this.productCoeff = productCoeff;
+		this.productMolecules = productMolecules;
+		this.reactantMasses = reactantMasses;
+		this.productMasses = productMasses;
+		limitingReagent = (this.getLimitingReagent()).get(0);
 	}
 	
 	protected ArrayList<String> condenseReaction(ArrayList<Integer> coefficient, ArrayList<String> molecules){
@@ -45,5 +61,38 @@ public class Reaction extends Molecule {
 		return condenseMolecule(c.getMoleculeArray());
 	}
 
+	protected boolean balancedReaction() {
+		ArrayList<String> reactants = new ArrayList<String>(condenseReaction(reactantCoeff,reactantMolecules));
+		ArrayList<String> products = new ArrayList<String>(condenseReaction(productCoeff,productMolecules));
+		for (int index = 0; index < reactants.size();index = index +2) {
+			int num = products.indexOf(reactants.get(index));
+			if (num >= 0 ) {
+				if (!(reactants.get(index+1).equals(products.get(num+1)))) return false;
+			} else return false;
+		}
+		return true;
+	}
+	
+	// returns an arraylist of [molecule, equivalency, index of its place in the instance variable arraylists]
+	private ArrayList<String> getLimitingReagent() {
+		int place = -1;
+		double lr = 0.0;
+		for (int index = 0; index < reactantMolecules.size();index ++) {
+			Molecule mol = new Molecule(reactantMolecules.get(index));
+			double equivalency = reactantMasses.get(index)/(mol.getMolecularWeight() * reactantCoeff.get(index));
+			if (lr == 0.0 || equivalency < lr) {
+				lr = equivalency;
+				place = index;
+			}
+		}
+		ArrayList<String> limreag = new ArrayList<String>();
+			limreag.add(reactantMolecules.get(place));
+			limreag.add(Double.toString(lr));
+			limreag.add(Integer.toString(place));
+		return limreag;
+	}
 
+	
+	
+	
 }
--- Group22Project/src/application/ReactionTester.java
+++ Group22Project/src/application/ReactionTester.java
@@ -2,44 +2,50 @@ package application;
 
 import java.util.ArrayList;
 
-public class ReactionTester extends Reaction{
-
-	public ReactionTester(ArrayList<Integer> reactantCoeff, ArrayList<String> reactantMolecules,
-			ArrayList<Integer> productCoeff, ArrayList<String> productMolecules) {
-		super(reactantCoeff, reactantMolecules, productCoeff, productMolecules);
-		// TODO Auto-generated constructor stub
-	}
-
+public class ReactionTester{
 	public static void main(String[] args) {
 		ArrayList<Integer> RCoeff = new ArrayList<Integer> ();
-		RCoeff.add(1);
 		RCoeff.add(2);
-		RCoeff.add(1);
-		RCoeff.add(2);
-		
+		RCoeff.add(8);
+		RCoeff.add(3);
+		//RCoeff.add(2);
 		ArrayList<Integer> PCoeff = new ArrayList<Integer> ();
 		PCoeff.add(1);
 		PCoeff.add(2);
 		PCoeff.add(1);
 		PCoeff.add(2);
-		
 		ArrayList<String> RMol = new ArrayList<String>();
-		RMol.add("CCC");
-		RMol.add("CCO");
-		RMol.add("CH3HH");
-		RMol.add("CH3");
-
+		RMol.add("BO3H3");
+		RMol.add("NH4F");
+		RMol.add("H2SO4");
+		//RMol.add("H3C");
 		ArrayList<String> PMol = new ArrayList<String>();
 		PMol.add("C");
 		PMol.add("C");
 		PMol.add("CH3");
-		PMol.add("CH3");
+		PMol.add("CH3");	
+		ArrayList<Double> reactMass = new ArrayList<Double>();
+		reactMass.add(1.018);
+		reactMass.add(2.512);
+		reactMass.add(5.0);
+		//reactMass.add(null);
+		ArrayList<Double> prodMass = new ArrayList<Double>();
+		prodMass.add(2.512);
+		prodMass.add(1.018);
+		prodMass.add(5.0);
+		//reactMass.add(null);
 		
-		Reaction reaction = new Reaction(RCoeff,RMol,PCoeff,PMol);
+		Reaction reaction = new Reaction(RCoeff,RMol,PCoeff,PMol,reactMass,prodMass);
 		ArrayList<String> test = reaction.condenseReaction(reaction.reactantCoeff,reaction.reactantMolecules);
 		System.out.println(test);
 		
-		//TODO condenseReaction does not work as planned. --> troubleshoot
+		ArrayList<String> test2 = reaction.condenseReaction(reaction.productCoeff,reaction.productMolecules);
+		System.out.println(test2);
+
+		boolean balanced = reaction.balancedReaction();
+		System.out.println(balanced);
+		
+		System.out.println(reaction.limitingReagent);
 	}
 
 }
