a297ac10a2c3cd585eb320b7a029519f067701b4
Macaella <macaella.roy@ucalgary.ca>
2022-11-09 23:37:11 -0700

    Reaction Class - FINISHED
    getter and setter methods added, and instance variables encapsulated.
    theoretical and percent yield methods added. JavaDocs added. still need
    to sort out first constructor

--- Group22Project/src/application/Reaction.java
+++ Group22Project/src/application/Reaction.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 
 public class Reaction extends Molecule {
 	// TODO encapsulate
-	ArrayList<Integer> reactantCoeff;
-	ArrayList<String> reactantMolecules;
-	ArrayList<Integer> productCoeff;
-	ArrayList<String> productMolecules;
-	ArrayList<Double> reactantMasses; 
-	ArrayList<Double> productMasses;
-	String limitingReagent;
+	private ArrayList<Integer> reactantCoeff;
+	private ArrayList<String> reactantMolecules;
+	private ArrayList<Integer> productCoeff;
+	private ArrayList<String> productMolecules;
+	private ArrayList<Double> reactantMasses; 
+	private ArrayList<Double> productMasses;
+	private ArrayList<String> limitingReagent;
 
+	/**
+	 * 
+	 * @param m
+	 */
 	public Reaction(String m) {
 		super(m);
 		// TODO make this actually sensible
 	}
+	/**
+	 * Constructor that takes in reactant and product coefficients and molecules
+	 * @param reactantCoeff
+	 * @param reactantMolecules
+	 * @param productCoeff
+	 * @param productMolecules
+	 */
 	public Reaction(ArrayList<Integer> reactantCoeff, ArrayList<String> reactantMolecules,
 			ArrayList<Integer> productCoeff,ArrayList<String> productMolecules) {
 		this.reactantCoeff = reactantCoeff;
@@ -24,9 +35,36 @@ public class Reaction extends Molecule {
 		this.productMolecules = productMolecules;
 		this.reactantMasses = new ArrayList<Double>();
 		this.productMasses = new ArrayList<Double>();
-		limitingReagent = "";
+		limitingReagent = new ArrayList<String>();
 	}
-	
+	/**
+	 * Constructor that takes in reactant and product coefficients and molecules, and reactant masses
+	 * @param reactantCoeff
+	 * @param reactantMolecules
+	 * @param productCoeff
+	 * @param productMolecules
+	 * @param reactantMasses
+	 */
+	public Reaction(ArrayList<Integer> reactantCoeff, ArrayList<String> reactantMolecules,
+			ArrayList<Integer> productCoeff,ArrayList<String> productMolecules,
+			ArrayList<Double> reactantMasses) {
+		this.reactantCoeff = reactantCoeff;
+		this.reactantMolecules = reactantMolecules;
+		this.productCoeff = productCoeff;
+		this.productMolecules = productMolecules;
+		this.reactantMasses = reactantMasses;
+		this.productMasses = new ArrayList<Double>();
+		limitingReagent = this.setLimitingReagent();
+	}
+	/**
+	 * Constructor that takes in reactant and product coefficients, molecules, and masses
+	 * @param reactantCoeff
+	 * @param reactantMolecules
+	 * @param productCoeff
+	 * @param productMolecules
+	 * @param reactantMasses
+	 * @param productMasses
+	 */
 	public Reaction(ArrayList<Integer> reactantCoeff, ArrayList<String> reactantMolecules,
 			ArrayList<Integer> productCoeff,ArrayList<String> productMolecules,
 			ArrayList<Double> reactantMasses, ArrayList<Double> productMasses) {
@@ -36,9 +74,56 @@ public class Reaction extends Molecule {
 		this.productMolecules = productMolecules;
 		this.reactantMasses = reactantMasses;
 		this.productMasses = productMasses;
-		limitingReagent = (this.getLimitingReagent()).get(0);
+		limitingReagent = this.setLimitingReagent();
+	}
+
+	ArrayList<Integer> getReactantCoeff(){
+		return reactantCoeff;
+	}
+	void setReactantCoeff(ArrayList<Integer> reactantCoeff) {
+		this.reactantCoeff = reactantCoeff;
+	}
+	ArrayList<String> getReactantMolecules(){
+		return reactantMolecules;
+	}
+	void setReactantMolecules(ArrayList<String> reactantMolecules) {
+		this.reactantMolecules = reactantMolecules;
+	}
+	ArrayList<Integer> getProductCoeff(){
+		return productCoeff;
+	}
+	void setProductCoeff(ArrayList<Integer> productCoeff) {
+		this.productCoeff = productCoeff;
+	}
+	ArrayList<String> getProductMolecules(){
+		return productMolecules;
+	}
+	void setproductMolecules(ArrayList<String> productMolecules) {
+		this.productMolecules = productMolecules;
+	}
+	ArrayList<Double> getReactantMasses(){
+		return reactantMasses;
+	}
+	void setReactantMasses(ArrayList<Double> reactantMasses) {
+		this.reactantMasses = reactantMasses;
+	}
+	ArrayList<Double> getProductMasses(){
+		return productMasses;
+	}
+	void setProductMasses(ArrayList<Double> productMasses) {
+		this.productMasses = productMasses;
+	}
+	ArrayList<String> getLimitingReagent(){
+		return limitingReagent;
 	}
 	
+	/**
+	 * Method that condenses a set of molecules so that there are no repeated atoms. The indexes of the molecules and 
+	 * their corresponding coefficients must align.
+	 * @param coefficient; the coefficients of the molecules to be condensed
+	 * @param molecules; the molecular formulas of the molecules to be condensed
+	 * @return the condensed set of molecules as a array of atoms (at even indexes) and the number of atoms (at odd indexes)
+	 */
 	protected ArrayList<String> condenseReaction(ArrayList<Integer> coefficient, ArrayList<String> molecules){
 		ArrayList<ArrayList<String>> condensedMol = new ArrayList<ArrayList<String>>();
 		for (String mol : molecules) {
@@ -61,6 +146,10 @@ public class Reaction extends Molecule {
 		return condenseMolecule(c.getMoleculeArray());
 	}
 
+	/**
+	 * Method that checks if a chemical equation is balanced
+	 * @return true if the equation is balanced and false if it is not
+	 */
 	protected boolean balancedReaction() {
 		ArrayList<String> reactants = new ArrayList<String>(condenseReaction(reactantCoeff,reactantMolecules));
 		ArrayList<String> products = new ArrayList<String>(condenseReaction(productCoeff,productMolecules));
@@ -73,23 +162,68 @@ public class Reaction extends Molecule {
 		return true;
 	}
 	
-	// returns an arraylist of [molecule, equivalency, index of its place in the instance variable arraylists]
-	private ArrayList<String> getLimitingReagent() {
-		int place = -1;
-		double lr = 0.0;
-		for (int index = 0; index < reactantMolecules.size();index ++) {
-			Molecule mol = new Molecule(reactantMolecules.get(index));
-			double equivalency = reactantMasses.get(index)/(mol.getMolecularWeight() * reactantCoeff.get(index));
-			if (lr == 0.0 || equivalency < lr) {
-				lr = equivalency;
-				place = index;
+	/**
+	 * Method evaluates the masses of the reagents and determines which one is the limiting reagent.
+	 * @return an array with the molecular formula of the LR, its equivalency (in moles), and its index in 
+	 * the reactantMolecules instance variable. If the reactant masses have not been assigned, an empty array is returned
+	 */
+	private ArrayList<String> setLimitingReagent() {
+		ArrayList<String> limreag = new ArrayList<String>();
+		if (!(reactantMasses.isEmpty())) {
+			int place = -1;
+			double lr = 0.0;
+			for (int index = 0; index < reactantMolecules.size();index ++) {
+				Molecule mol = new Molecule(reactantMolecules.get(index));
+				double equivalency = reactantMasses.get(index)/(mol.getMolecularWeight() * reactantCoeff.get(index));
+				if (lr == 0.0 || equivalency < lr) {
+					lr = equivalency;
+					place = index;
+				}
+			}
+				limreag.add(reactantMolecules.get(place));
+				limreag.add(Double.toString(lr));
+				limreag.add(Integer.toString(place));
+			return limreag;
+		}else {
+			return limreag;
+		}
+	}
+	
+	/**
+	 * Determines the theoretical yield of each product based on the limiting reagent equivalency.
+	 * @return an array with the product molecular formula at even indexes and its theoretical yield 
+	 * at the following odd indexes. If there is no LR, an empty array is returned
+	 */
+	protected ArrayList<String> theoreticalYield(){
+		ArrayList<String> productTheorYield = new ArrayList<String>();
+		if (!(limitingReagent.isEmpty())) {
+			for (int index = 0; index < productMolecules.size();index ++) {
+				Molecule mol = new Molecule(productMolecules.get(index));
+				double ty = (Double.parseDouble(limitingReagent.get(1)))*productCoeff.get(0)*mol.getMolecularWeight();
+				productTheorYield.add(productMolecules.get(index));
+				productTheorYield.add(Double.toString(ty));
 			}
 		}
-		ArrayList<String> limreag = new ArrayList<String>();
-			limreag.add(reactantMolecules.get(place));
-			limreag.add(Double.toString(lr));
-			limreag.add(Integer.toString(place));
-		return limreag;
+		return productTheorYield;
+	}
+	
+	/**
+	 * Calculates the percent yield for each product based on the theoretical yield and the given masses of products
+	 * @return an array with the product molecular formula at even indexes and its percent yield 
+	 * at the following odd indexes. If there is no theoretical yield or product masses, an empty array is returned
+	 */
+	protected ArrayList<String> yieldPercent(){
+		ArrayList<String> productYieldPercent = new ArrayList<String>();
+		if (!(productMasses.isEmpty()) && !(limitingReagent.isEmpty())) {
+			ArrayList<String> productTheorYield = theoreticalYield();
+			int count = 0;
+			for (int index = 1; index < productTheorYield.size(); index = index +2) {
+				productYieldPercent.add(productTheorYield.get(index-1));
+				productYieldPercent.add(Double.toString(productMasses.get(count)/Double.parseDouble(productTheorYield.get(index))));
+				count ++;
+			}
+		return productYieldPercent;
+		}else return productYieldPercent;
 	}
 
 	
--- Group22Project/src/application/ReactionTester.java
+++ Group22Project/src/application/ReactionTester.java
@@ -10,42 +10,45 @@ public class ReactionTester{
 		RCoeff.add(3);
 		//RCoeff.add(2);
 		ArrayList<Integer> PCoeff = new ArrayList<Integer> ();
-		PCoeff.add(1);
 		PCoeff.add(2);
-		PCoeff.add(1);
-		PCoeff.add(2);
+		PCoeff.add(3);
+		PCoeff.add(6);
+		//PCoeff.add(2);
 		ArrayList<String> RMol = new ArrayList<String>();
 		RMol.add("BO3H3");
 		RMol.add("NH4F");
 		RMol.add("H2SO4");
 		//RMol.add("H3C");
 		ArrayList<String> PMol = new ArrayList<String>();
-		PMol.add("C");
-		PMol.add("C");
-		PMol.add("CH3");
-		PMol.add("CH3");	
+		PMol.add("NH4BF4");
+		PMol.add("N2H8SO4");
+		PMol.add("H2O");
+		//PMol.add("CH3");	
 		ArrayList<Double> reactMass = new ArrayList<Double>();
 		reactMass.add(1.018);
 		reactMass.add(2.512);
 		reactMass.add(5.0);
 		//reactMass.add(null);
 		ArrayList<Double> prodMass = new ArrayList<Double>();
-		prodMass.add(2.512);
+		prodMass.add(1.460);
 		prodMass.add(1.018);
 		prodMass.add(5.0);
 		//reactMass.add(null);
 		
-		Reaction reaction = new Reaction(RCoeff,RMol,PCoeff,PMol,reactMass,prodMass);
-		ArrayList<String> test = reaction.condenseReaction(reaction.reactantCoeff,reaction.reactantMolecules);
+		Reaction reaction = new Reaction(RCoeff,RMol,PCoeff,PMol);
+		ArrayList<String> test = reaction.condenseReaction(reaction.getReactantCoeff(),reaction.getReactantMolecules());
 		System.out.println(test);
 		
-		ArrayList<String> test2 = reaction.condenseReaction(reaction.productCoeff,reaction.productMolecules);
+		ArrayList<String> test2 = reaction.condenseReaction(reaction.getProductCoeff(),reaction.getProductMolecules());
 		System.out.println(test2);
 
 		boolean balanced = reaction.balancedReaction();
 		System.out.println(balanced);
 		
-		System.out.println(reaction.limitingReagent);
+		System.out.println(reaction.getLimitingReagent());
+		System.out.println(reaction.theoreticalYield());
+		System.out.println(reaction.yieldPercent());
+
 	}
 
 }
